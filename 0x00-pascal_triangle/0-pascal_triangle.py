030f2d5c400db924eb15cf2f937112948a8ce6c2
manodhiambo <manodhiambo@gmail.com>
2022-06-15 16:28:10 +0000

    Fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 
+#!/usr/bin/python3
 """
-pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
+Returns a list if n <= 0
+You can assume n will always be an integer
+"""
 
-def print_triangle(triangle):
-"""
+
 def pascal_triangle(n):
-   trow = [1]
-   y = [0]
-   for x in range(max(n,0)):
-      print(trow)
-      trow=[l+r for l,r in zip(trow+y, y+trow)]
-   return n>=1
-pascal_triangle(6) 
-
+    """
+    print a list that will make a triangle
+    """
+    triangle = []
+    row = []
+    result = []
+    if n <= 0:
+        return []
+    for i in range(0, n + 1):
+        row = [j > 0 and j < i - 1 and i > 2 and result[j - 1] +
+               result[j] or 1 for j in range(0, i)]
+        result = row
+        triangle += [row]
+    return triangle[1:]
